919dda7d257135c028f771be4d0e33bb5553ba09
KoolKid6958 <156125488+KoolKid6958@users.noreply.github.com>
2025-06-06 18:40:48 -0400

    Added start for a close confirmation box

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // makes the terminal not show on windows
 
 slint::include_modules!();
+
 fn main() -> Result<(), slint::PlatformError> {
     let main_window = MainWindow::new()?;
     let time_elapsed: String = String::from("0s");
@@ -11,6 +12,7 @@ fn main() -> Result<(), slint::PlatformError> {
     let frames_left: String = String::from("0");
     let sheepit_version: String = String::from("7.25091.0");
     let manager_version: String = String::from("0.1.0");
+
     //Sets all the values in the slint file to the values in rust
     main_window
         .global::<values>()
@@ -37,5 +39,13 @@ fn main() -> Result<(), slint::PlatformError> {
         .global::<values>()
         .set_manager_version(manager_version.into());
 
+    //Logic for when the window closes
+    let window = main_window.window();
+
+    window.on_close_requested(|| {
+        println!("Will add a confirmation box soon");
+        slint::CloseRequestResponse::HideWindow
+    });
     main_window.run()
+    
 }
